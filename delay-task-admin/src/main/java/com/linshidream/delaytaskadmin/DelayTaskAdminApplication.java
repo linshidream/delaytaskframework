b7bfb08dd0af84fababc7d9e02b1b3e754f3fb01
linshidream <zhengxing0117@foxmail.com>
2025-05-16 16:18:17 +0800

    Initial commit

---
+++ delay-task-admin/src/main/java/com/linshidream/delaytaskadmin/DelayTaskAdminApplication.java
@@ -0,0 +1,13 @@
+package com.linshidream.delaytaskadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayTaskAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelayTaskAdminApplication.class, args);
+    }
+
+}
